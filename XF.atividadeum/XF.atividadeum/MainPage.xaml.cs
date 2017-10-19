96d74faf3428038d34e3f1ae189bf543cd8068f2
aledebarba <ale@alemacedo.com>
2017-10-18 22:51:43 -0200

    Atividade 1 tentativa 2

---
+++ XF.atividadeum/XF.atividadeum/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.atividadeum
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EnviarClick(object sender, EventArgs e) { }
+        private void ConfigurarClick(object sender, EventArgs e) { }
+
+    }
+}
